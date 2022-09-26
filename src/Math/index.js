a5c42f9f02e3e7695f7c82c3e99acd852a7fad77
Max Levytskyi <immaxlev@gmail.com>
2022-09-26 19:12:05 +0300

    feat: Math

--- src/Math/index.js
+++ src/Math/index.js
@@ -1,3 +1,9 @@
+/**
+ * Returns a random integer
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 export function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -10,3 +16,23 @@ export function lerp(a, b, t) {
 }
 
 export const PI_OVER_180 = Math.PI / 180;
+
+export function isInt(number) {
+  return Number(number) === number && number % 1 === 0;
+}
+
+export function isFloat(number) {
+  return Number(number) === number && number % 1 !== 0;
+}
+
+export function isNumber(number) {
+  return isInt(number) || isFloat(number);
+}
+
+export function toNumber(string) {
+  if (isNumber(string)) {
+    return string;
+  }
+  const number = Number(string);
+  return isNumber(number) ? number : 0;
+}
