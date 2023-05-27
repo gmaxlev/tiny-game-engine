8ccc083f547d9001496165b67a90f648bc730906
Max Levytskyi <immaxlev@gmail.com>
2023-05-27 19:01:38 +0300

    feat: divided tsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 
-export default defineConfig({
-  plugins: [
-    checker({
-      typescript: true,
-      lintCommand: 'eslint "./src/**/*.{ts}"'
-    })
-  ]
+export default defineConfig((a) => {
+  const IS_PRODUCTION = a.mode === 'production'
+
+  const tsConfig = IS_PRODUCTION ? 'tsconfig.prod.json' : 'tsconfig.dev.json'
+
+  return {
+    plugins: [
+      checker({
+        typescript: {
+          tsconfigPath: tsConfig
+        },
+        lintCommand: 'eslint "./src/**/*.{ts}"'
+      })
+    ]
+  }
 })
